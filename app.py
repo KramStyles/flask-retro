89ccba76b4479585a9b9524c3ffd2d7e752fc573
kramstyles <mark.eke@decagon.dev>
2022-03-10 23:14:38 +0100

    basic route

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+app.env = 'development'
+
+@app.route('/')
+def home():
+    return '<h2>Welcome to Flask Intro</h2>'
+
 if __name__ == '__main__':
-    print('App running')
+    app.run(debug=True)
