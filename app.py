94d4ea82fae8d24d5489fe22888522f1b8cc63ed
kramstyles <mark.eke@decagon.dev>
2022-03-12 08:13:57 +0100

    auto-fit and minmax

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def home():
 def grid():
     return render_template('grid_1.html')
 
+@app.route('/grid_2')
+def grid_2():
+    return render_template('grid_2.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
